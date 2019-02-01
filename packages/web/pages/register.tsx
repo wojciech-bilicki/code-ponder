e60a538493188e7ef5c79eb200624d8086d7a23c
Wojciech Bilicki <wojciech.bilicki@oke.com>
2019-02-01 09:54:02 +0100

    End of part 1

---
+++ packages/web/pages/register.tsx
@@ -0,0 +1,10 @@
+import Link from 'next/link'
+import Layout from '../components/Layout';
+
+export default () => (
+  <Layout title="About | Next.js + TypeScript Example">
+    <div>
+      register
+    </div>
+  </Layout>
+)
